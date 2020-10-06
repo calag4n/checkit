6680f604f9847decd1d4bc66f0f3ca276533c3e0
calag4n <console.logan@gmail.com>
2020-10-06 14:39:55 +0200

    removed plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,20 +14,6 @@ module.exports = {
         lang: 'fr'
       }
     },
-    {
-      resolve: "gatsby-plugin-firebase",
-      options: {
-        credentials: {
-          apiKey: "AIzaSyCFIuE2-ljTvqC65uwDALHibAEwXeuXuho",
-          authDomain: "checkit-1a1c5.firebaseapp.com",
-          databaseURL: "https://checkit-1a1c5.firebaseio.com",
-          projectId: "checkit-1a1c5",
-          storageBucket: "checkit-1a1c5.appspot.com",
-          messagingSenderId: "126717325093",
-          appId: "1:126717325093:web:9e5056206355dad2d9b4cc",
-        }
-      }
-    },
 
     `gatsby-plugin-react-helmet`,
     {
